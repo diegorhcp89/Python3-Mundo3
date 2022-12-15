c97c0fb86bb6c9a055b65cbd8627eb0bec55509f
Diego Ferreira do Amaral <diegoferreiradoamaral@gmail.com>
2022-12-15 15:55:39 -0300

    Adicionando aula 23 Tratamento de Errors e Execeções

---
+++ Aulas/Aula - 23 Tratamento de Erros e Exceções.py
@@ -0,0 +1,11 @@
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominado: '))
+    r = a / b
+except Exception as erro:
+    print(f'Problema encontrado foi {erro.__class__}')
+else:
+    print(f'O resultado é {r:.1f}')
+finally:
+    print('Volte sempre! Muito Obrigado!')
+    
